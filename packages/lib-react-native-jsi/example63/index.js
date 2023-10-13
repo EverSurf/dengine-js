47083d50e75f531845348f25699e382d63829930
Nikita.m <nikita.m@tonlabs.io>
2023-10-13 18:36:51 +0300

    Fix example63

--- packages/lib-react-native-jsi/example63/index.js
+++ packages/lib-react-native-jsi/example63/index.js
@@ -5,10 +5,13 @@
 import App from './App';
 import {AppRegistry} from 'react-native';
 import {TonClient} from '@eversdk/core';
+import {DebotClient} from '@eversurf/dengine';
 import {name as appName} from './app.json';
 import {libReactNativeJsi} from '@eversdk/lib-react-native-jsi';
+import {libReactNativeJsi as libDengineJsi} from '@eversurf/dengine-rn-jsi';
 
 // eslint-disable-next-line react-hooks/rules-of-hooks
 TonClient.useBinaryLibrary(libReactNativeJsi);
+DebotClient.useBinaryLibrary(libDengineJsi);
 
 AppRegistry.registerComponent(appName, () => App);
